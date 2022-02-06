f4695adcd69efb9be3c81ac6af2cf2e8bc0da1ae
MoBough <boughdiri.mo@gmail.com>
2022-02-06 14:44:23 +0000

    troisieme modif

--- src/main/java/com/sip/ams/Controllers/HomeController.java
+++ src/main/java/com/sip/ams/Controllers/HomeController.java
@@ -1,15 +1,30 @@
 package com.sip.ams.Controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/info") //ce que je tape dans l'url	
-	public String info()
+	public String info(Model model)
 	{
+		String formation = "Fullstack";
+		
+		
+		ArrayList<String> mylist = new ArrayList<String>();
+		mylist.add("OCP");
+	    mylist.add("OCP");
+	    mylist.add("Spring");
+	    mylist.add("Angular");
+		
+		
 		System.out.println("Methode info");
+		model.addAttribute("workshop", formation);
+		model.addAttribute("workshop2", mylist);
 		return "home/info"; //le nom de la vue sous le dossier template
 	}
 	
